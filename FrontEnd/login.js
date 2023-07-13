ea2a8e0a8d50678ddc122e67d23f8c645d166a39
shenzor <toppan.frederic@gmail.com>
2023-07-13 23:51:16 +0200

    integration du design de la page login

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,5 +1,3 @@
-//import callApi from "./script.js";
-
 const url = "http://localhost:5678/api/works";
 //création d'une variable init
 const init = {
@@ -25,22 +23,29 @@ async function callApi() {
 //effacement de la zone introduction et portfolio
 document.querySelector("#introduction").remove();
 document.querySelector("#portfolio").remove();
-console.log(callApi);
 
 //vérification si j'interviens bien sur le DOM
-// document.write("ca marche");
+//document.write("ca marche");
 
 //création du bouton se connecter
 const buttons = document.createElement("div");
 // Ajout d'une classe pour le style CSS
 buttons.classList.add("buttons");
-
+buttons.style.paddingTop = "30px";
 const seConnecter = document.createElement("button");
 seConnecter.textContent = "Se connecter";
-
 buttons.appendChild(seConnecter);
 //position du bouton après la zone input du login
 document.querySelector("#contact").append(buttons);
 
-///////////////////////// voir onclick dans js...
-//document.querySelector(seConnecter).style.marginBottom='20px';
+const lostMdpBlock = document.createElement("div");
+const lostMdp = document.createElement("a");
+lostMdp.href = "./index.html";
+lostMdp.textContent = "Mot de passe oublié";
+contact.appendChild(lostMdpBlock);
+lostMdpBlock.appendChild(lostMdp);
+
+let a = document.querySelector("a");
+a.onclick = () => {
+  return confirm("Mot de passe Oublié");
+};
